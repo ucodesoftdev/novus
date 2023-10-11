e0ad407b111261d8665d941d4ff8f86146f6e7da
ravi-ranjan-kumar <120107346+ravi-ranjan-kumar@users.noreply.github.com>
2023-10-11 11:12:00 +0530

    Update location-v4.js

--- scripts/location-v4.js
+++ scripts/location-v4.js
@@ -9,51 +9,45 @@ $(document).ready(function () {
     $(this).find(".location-menu").slideUp(300);
   });
 });
-function initMap(event, category = null) {
-  const buttons = document.querySelectorAll(".sec-loca-map-comp-button");
-  const list = document.querySelectorAll(".location-menu li");
-  if (buttons || list) {
-    for (const button of buttons) {
-      button.style.backgroundColor = "#37382b";
-    }
-    for (const li of list) {
-      li.style.backgroundColor = "#37382b";
-    }
-  }
-  if (event) {
-    event.currentTarget.style.backgroundColor = "rgba(115, 120, 89, 0.5)";
-  }
-  indexInfoWindow = 0;
-  const section = document.querySelector(".location-map");
-  const novusMapElement = document.querySelector(".sec-location-v4-5");
 
-  if (!section || !novusMapElement) {
-    console.error("Map container not found!");
+const colorCode = {
+  art: "#5684b9",
+  cafe: "#c87474",
+  bar: "#926dc1",
+  local_treasure: "#e7c28c",
+};
+const icons = {
+  art: {
+    default: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac0fc130beb68ae85d902_art-marker.svg`,
+    active: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac3fa40e06da62f7a5a2f_art-active-marker.svg`,
+  },
+  cafe: {
+    default: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac0fc40e06da62f7770e4_cafe-marker.svg`,
+    active: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac3f9f964c08e55a37139_active-cafe-marker.svg`,
+  },
+  bar: {
+    default: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac0fbed3778abead795a4_bar-marker.svg`,
+    active: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac3fa861b944ecf8c2bde_active-bar-marker.svg`,
+  },
+  local_treasure: {
+    default: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac0fbed3778abead795a1_local-treasure-marker.svg`,
+    active: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac3fa9681e98c08573f2d_active-local-treasure-marker.svg`,
+  },
+};
+const allMarkers = [];
+let currentTabMarkers = [];
+let map;
+
+function initMap() {
+  const mapContainer = document.querySelector(".location-map");
+  if (!mapContainer) {
+    console.error("Unable to load Map");
     return;
   }
 
-  let latLong = { lat: -37.8278762, lng: 144.9665503 };
-
-  if (window.screen.width > 1440) {
-    latLong.lng += 0.06;
-  }
-  if (window.screen.width >= 1351 && window.screen.width <= 1440) {
-    latLong.lng += 0.04;
-  }
-  if (window.screen.width < 1024) {
-    latLong.lat -= 0.02;
-  }
-  const novusMap = new google.maps.Map(novusMapElement, {
-    center: latLong,
-    zoom: 13.5,
-    disableDefaultUI: true,
-    mapId: "4e7898bbcab92cd0",
-  });
-
-  createNovusMarker(novusMap);
-  const map = new google.maps.Map(section, {
+  map = new google.maps.Map(mapContainer, {
     center: { lat: -37.8278762, lng: 144.9665503 },
-    zoom: 16,
+    zoom: 18,
     disableDefaultUI: true,
     styles: [
       {
@@ -140,104 +134,96 @@ function initMap(event, category = null) {
       },
     ],
   });
-  callback(category, map, novusMap);
-  service = new window.google.maps.places.PlacesService(map);
-}
 
-async function callback(category, map, novusMap) {
-  const colorCode = {
-    art: "#5684b9",
-    cafe: "#c87474",
-    bar: "#926dc1",
-    local_treasure: "#e7c28c",
-  };
-  const icons = {
-    art: {
-      default: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac0fc130beb68ae85d902_art-marker.svg`,
-      active: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac3fa40e06da62f7a5a2f_art-active-marker.svg`,
-    },
-    cafe: {
-      default: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac0fc40e06da62f7770e4_cafe-marker.svg`,
-      active: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac3f9f964c08e55a37139_active-cafe-marker.svg`,
-    },
-    bar: {
-      default: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac0fbed3778abead795a4_bar-marker.svg`,
-      active: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac3fa861b944ecf8c2bde_active-bar-marker.svg`,
-    },
-    local_treasure: {
-      default: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac0fbed3778abead795a1_local-treasure-marker.svg`,
-      active: `https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/650ac3fa9681e98c08573f2d_active-local-treasure-marker.svg`,
-    },
-  };
-  const bounds = new google.maps.LatLngBounds();
-  if (!locations || locations?.length < 1) {
-    console.log("No Results Found");
+  createMarkers(map);
+
+  //Novus map integration
+
+  const novusMapElement = document.querySelector(".sec-location-v4-5");
+  if (!novusMapElement) {
+    console.error("Unable to load the Novus Map");
     return;
   }
-  let results = locations;
-  if (category) {
-    results = locations.filter((location) => location.type === category);
+
+  let latLong = { lat: -37.8278762, lng: 144.9665503 };
+
+  if (window.screen.width > 1440) {
+    latLong.lng += 0.06;
   }
+  if (window.screen.width >= 1351 && window.screen.width <= 1440) {
+    latLong.lng += 0.04;
+  }
+  if (window.screen.width < 1024) {
+    latLong.lat -= 0.02;
+  }
+
+  const novusMap = new google.maps.Map(novusMapElement, {
+    center: latLong,
+    zoom: 13.5,
+    disableDefaultUI: true,
+    mapId: "4e7898bbcab92cd0",
+  });
+  createNovusMarker(novusMap);
+}
+
+function createMarkers(map) {
+  const bounds = new google.maps.LatLngBounds();
+
   const defaultMarker = new google.maps.Marker({
     map,
     position: { lat: -37.8278762, lng: 144.9665503 },
     icon: "https://uploads-ssl.webflow.com/6227f17380fa37ea2192faa4/65082b1e3487e4f9f004f556_location-marker.svg",
   });
   bounds.extend(defaultMarker.position);
-  const markers = [];
-  for (let i = 0; i < results.length; i++) {
-    getLatLong(`${results[i].title} ${results[i].address}`).then(
-      (locationResult) => {
-        createMarker(
-          i,
-          results[i],
-          map,
-          bounds,
-          markers,
-          colorCode,
-          icons,
-          locationResult
-        );
 
-        if (i === results.length - 1) {
-          if (window.screen.width <= 1024) {
-            map.fitBounds(bounds, { top: 300 });
-            return;
-          }
-          map.fitBounds(bounds);
-        }
+  for (let i = 0; i < locations.length; i++) {
+    const currentLocation = locations[i];
+
+    console.log("location", locations[i]);
+
+    const markerLatLng = {
+      lat: locations[i]?.lat,
+      lng: locations[i]?.lng,
+    };
+    const marker = new google.maps.Marker({
+      map,
+      position: markerLatLng,
+      icon: icons[currentLocation.type].default,
+    });
+
+    google.maps.event.addListener(marker, "click", () => {
+      for (let i = 0; i < allMarkers.length; i++) {
+        allMarkers[i].marker.setIcon(icons[allMarkers[i].type].default);
       }
-    );
-  }
-}
 
-async function createMarker(index, place, map, bounds, markers, colorCode, icons, locationResult) {
-  const markerLatLng = {
-    lat: locationResult?.geometry?.location.lat(),
-    lng: locationResult?.geometry?.location.lng(),
-  };
-  
-  const marker = new google.maps.Marker({
-    map,
-    position: markerLatLng,
-    icon: icons[place.type].default,
-  });
-  
-  markers.push({ marker, type: place.type });
-  bounds.extend(marker.position);
+      marker.setIcon(icons[currentLocation.type].active);
+      openInfoWindow({
+        type: currentLocation.type,
+        title: currentLocation.title,
+        address: currentLocation.address,
+      });
+    });
 
-  if (index === 0 && markers[0]) {
-    openInfoWindow(place, colorCode, markers, locationResult?.place_id);
-    markers[0].marker.setIcon(icons[place.type].active);
-  }
-  
-  google.maps.event.addListener(marker, "click", () => {
-    for (let i = 0; i < markers.length; i++) {
-      markers[i].marker.setIcon(icons[markers[i].type].default);
+    bounds.extend(marker.position);
+    allMarkers.push({ marker, type: currentLocation.type });
+    currentTabMarkers.push({ marker, type: currentLocation.type });
+
+    if (i === 0) {
+      openInfoWindow({
+        type: currentLocation.type,
+        title: currentLocation.title,
+        address: currentLocation.address,
+      });
     }
-    marker.setIcon(icons[place.type].active);
-    openInfoWindow(place, colorCode, markers, locationResult?.place_id);
-  });
+
+    if (i === locations.length - 1) {
+      if (window.screen.width <= 1024) {
+        map.fitBounds(bounds, { top: 300 });
+        return;
+      }
+      map.fitBounds(bounds);
+    }
+  }
 }
 
 function createNovusMarker(map) {
@@ -305,9 +291,43 @@ function createNovusMarker(map) {
   novusMapElement.parentNode.append(infoWindow);
 }
 
-const openInfoWindow = async (location, colorCode, markers, place_id) => {
+function handleTabChange(event, tab) {
+  const tabs = document.querySelectorAll("[data-selected]");
+
+  if (tabs && event) {
+    for (const tab of tabs) {
+      tab.setAttribute("data-selected", "false");
+    }
+    event.currentTarget.setAttribute("data-selected", "true");
+  }
+
+  currentTabMarkers = [];
+  indexInfoWindow = 0;
+  const bounds = new google.maps.LatLngBounds();
+
+  allMarkers.forEach(({ type, marker }, index) => {
+    if (type === tab) {
+      bounds.extend(marker.position);
+      marker.setVisible(true);
+      currentTabMarkers.push({ type, marker });
+    } else {
+      marker.setVisible(false);
+    }
+  });
+  new google.maps.event.trigger(
+    currentTabMarkers[indexInfoWindow].marker,
+    "click"
+  );
+  if (window.screen.width <= 1024) {
+    map.fitBounds(bounds, { top: 300 });
+    return;
+  }
+  map.fitBounds(bounds);
+}
+
+const openInfoWindow = async (location) => {
   const locationMap = document.querySelector(".location-map");
-  let owlContainer = document.querySelector(".owl-carousel");
+  let carouselContainer = document.querySelector(".owl-carousel");
   let infoDiv = document.querySelector(".location-data");
 
   if (!locationMap) {
@@ -320,33 +340,33 @@ const openInfoWindow = async (location, colorCode, markers, place_id) => {
 
   infoDiv.classList.add("location-data");
 
-  const images = await getPlaceId(place_id);
+  const images = await getImageByPlaceId(location.place_id);
 
-  owlContainer = "";
+  carouselContainer = "";
 
   images.forEach((image) => {
-    owlContainer += `
-      <div class="owl-item-image-box item">
-        <img src=${image}/>
-      </div>
-    `;
+    carouselContainer += `
+        <div class="owl-item-image-box item">
+          <img src=${image}/>
+        </div>
+      `;
   });
 
   infoDiv.innerHTML = `
-    <div class="info-image-div">
-      <div class="owl-carousel">
-        ${owlContainer}
+      <div class="info-image-div">
+        <div class="owl-carousel">
+          ${carouselContainer}
+        </div>
       </div>
-    </div>
-    <div class="info-heading">
-      <h3 class="info-title">${location.title}</h3>
-      <div class="info-navigation prev_next">
-        <svg class="nav-prev" style="height:23px;width:23px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><polyline fill="none" stroke="currentColor" stroke-width="2" points="7 2 17 12 7 22" transform="matrix(-1 0 0 1 24 0)"></polyline></svg>
-        <p class="info-description">${location.address}</p>
-        <svg class="nav-next" style="height:23px;width:23px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><polyline fill="none" stroke="currentColor" stroke-width="2" points="7 2 17 12 7 22"></polyline></svg>
+      <div class="info-heading">
+        <h3 class="info-title">${location.title}</h3>
+        <div class="info-navigation prev_next">
+          <svg class="nav-prev" style="height:23px;width:23px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><polyline fill="none" stroke="currentColor" stroke-width="2" points="7 2 17 12 7 22" transform="matrix(-1 0 0 1 24 0)"></polyline></svg>
+          <p class="info-description">${location.address}</p>
+          <svg class="nav-next" style="height:23px;width:23px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><polyline fill="none" stroke="currentColor" stroke-width="2" points="7 2 17 12 7 22"></polyline></svg>
+        </div>
       </div>
-    </div>
-  `;
+    `;
 
   infoDiv.style.setProperty("--main-bg-color", colorCode[location.type]);
   locationMap.parentNode.insertBefore(infoDiv, locationMap.nextSibling);
@@ -355,27 +375,34 @@ const openInfoWindow = async (location, colorCode, markers, place_id) => {
   const navPrev = document.querySelector(".nav-prev");
   if (navNext && navPrev) {
     navNext.onclick = function () {
-      if (markers.length - 1 === indexInfoWindow) {
+      if (currentTabMarkers.length - 1 === indexInfoWindow) {
         indexInfoWindow = 0;
       } else {
         indexInfoWindow++;
       }
-      new google.maps.event.trigger(markers[indexInfoWindow].marker, "click");
+      new google.maps.event.trigger(
+        currentTabMarkers[indexInfoWindow].marker,
+        "click"
+      );
     };
     navPrev.onclick = function () {
       if (0 === indexInfoWindow) {
-        indexInfoWindow = markers.length - 1;
+        indexInfoWindow = currentTabMarkers.length - 1;
       } else {
         indexInfoWindow--;
       }
-      new google.maps.event.trigger(markers[indexInfoWindow].marker, "click");
+      new google.maps.event.trigger(
+        currentTabMarkers[indexInfoWindow].marker,
+        "click"
+      );
     };
   }
   getOwlCarousel($(".owl-carousel"), colorCode[location.type]);
 };
 
-async function getPlaceId(place_id) {
+async function getImageByPlaceId(place_id) {
   try {
+    throw Error("fdlkgjfd;gjkdfjglkjdfl");
     if (!place_id) {
       console.info("Address not found");
       return null;
@@ -404,25 +431,9 @@ async function getPlaceId(place_id) {
   }
 }
 
-async function getLatLong(address) {
-  try {
-    const geocoder = new google.maps.Geocoder();
-    const { results } = await geocoder.geocode({ address: address });
-    if (!results || !results.length) {
-      console.info("Address not found");
-      return null;
-    }
-
-    return results[0];
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
-
-function getOwlCarousel(element, color="#eec485") {
+function getOwlCarousel(element, color = "#eec485") {
   if (!element) {
-    console.log("carousel element not found");
+    console.error("carousel element not found");
     return;
   }
   element.owlCarousel({
@@ -436,8 +447,37 @@ function getOwlCarousel(element, color="#eec485") {
       },
     },
     navText: [
-      `<svg style="width:20px; height:20px" class="w-64 h-64" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><polyline fill="none" stroke="${color}" stroke-width="2" points="7 2 17 12 7 22" transform="matrix(-1 0 0 1 24 0)"></polyline></svg>`,
-      `<svg style="width:20px; height:20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff"><polyline fill="none" stroke="${color}" stroke-width="2" points="7 2 17 12 7 22"></polyline></svg>`,
+      `<svg style="width:20px; height:20px" class="w-64 h-64" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><polyline fill="none" stroke="${color}" stroke-width="3" points="7 2 17 12 7 22" transform="matrix(-1 0 0 1 24 0)"></polyline></svg>`,
+      `<svg style="width:20px; height:20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff"><polyline fill="none" stroke="${color}" stroke-width="3" points="7 2 17 12 7 22"></polyline></svg>`,
     ],
   });
 }
+
+async function getLatLong(address) {
+  try {
+    const geocoder = new google.maps.Geocoder();
+    const { results } = await geocoder.geocode({ address: address });
+    if (!results || !results.length) {
+      console.info("Address not found");
+      return null;
+    }
+
+    console.log("markers", results[0]);
+    return results[0];
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+$(document).ready(function () {
+  $(".location-filter").click(function () {
+    $(this).attr("tabindex", 1).focus();
+    $(this).toggleClass("active");
+    $(this).find(".location-menu").slideToggle(300);
+  });
+  $(".location-filter").focusout(function () {
+    $(this).removeClass("active");
+    $(this).find(".location-menu").slideUp(300);
+  });
+});
